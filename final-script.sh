c23fbccba18d61afa09c936da5a5b2bf9d710ad4
Victor Melly Cabredo <vmcabredo@ibm.com>
2023-01-19 11:24:24 -0300

    Update final-script.sh

--- final-script.sh
+++ final-script.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 source ./script.conf
+
+#Retrieve the oauth token
 source ./ic_oauth_token.sh $API_KEY > oauth-token.json
-python oauth-token-parser.py oauth-token.json
+python3 oauth-token-parser.py oauth-token.json
 OAUTH_TOKEN=$(cat oauth-token.txt)
 
 #List buckets
 source ./list-buckets.sh $API_KEY $CRN $COS_ENDPOINT > bucket-list.xml
+python3 buckets-xml-parser.py bucket-list.xml
 
-python buckets-xml-parser.py bucket-list.xml
-
-#List every object in every bucket in bucket-list.txt
+#Iterate every bucket in buckets.txt
 for bucket in $(cat buckets.txt)
 do
+	#List all objects from a bucket
 	source ./list-objects.sh $OAUTH_TOKEN $COS_ENDPOINT $bucket \
 	> $bucket-objects.xml
 
 	#Parse desired objects and save to txt
-	python object-xml-parser.py $bucket-objects.xml $DATE
+	python3 object-xml-parser.py $bucket-objects.xml $DATE
 	
-	#Restore all objects in the bucket
+	#Restore all desired objects in the bucket
 	for object in $(cat $bucket-objects.txt)
 	do
 		source ./restore-object.sh $OAUTH_TOKEN $COS_ENDPOINT \
